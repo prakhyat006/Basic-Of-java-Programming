22409bf747e9df1539252ff0e45d9a3505c91367
prakhyat006 <prakhyatshetty710@gmail.com>
2025-05-20 14:00:07 +0530

    Done all

--- Lab3StringOperations/strings/CheckNumericString.java
+++ Lab3StringOperations/strings/CheckNumericString.java
@@ -1,29 +1,31 @@
 /*
- Q7. Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user
-defined function truncate()
+ Q8. Write a Java Program for Verifying if a string contains only numeric characters using user defined
+function isNumeric()
  */
 package strings;
 
 public class CheckNumericString {
-		    public static String truncate(String str, int maxLength) {
-		        if (str == null) {
-		            return null;
-		        }
-		        if (str.length() <= maxLength) {
-		            return str;
-		        }
-				// this is used to truncate the string  
-		        else {
-		            return str.substring(0, maxLength) + "...";
-		        }
-		    }
-
-		    public static void main(String[] args) {
-		        String str= "hello i prakhyat shetty and today is monday";
-		        int maxLength = 10;
-		        System.out.println("Original: " +str);
-		        System.out.println("Truncated to " + maxLength + ": " + truncate(str, maxLength) ); 
-		    }
+	  public static boolean isNullOrEmpty(String str) {
+	        return str == null || str.trim().isEmpty();
+	    }
+	  public static boolean isNumeric(String str) {
+	        if (isNullOrEmpty(str)) {
+	            return false;
+	        }
+			// converts it to numeric values 
+	        for (int i = 0; i < str.length(); i++) {
+	            if (!Character.isDigit(str.charAt(i))) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+	    public static void main(String[] args) {
+	        String str1 = "12345";
+	      String str2 ="prakhyat";
+	        System.out.println(str1 + "' is numeric: " + isNumeric(str1)); 
+	        System.out.println(str2 + "' is numeric: " + isNumeric(str2)); 
+	    }
+	   
+	  
 }
-
-
